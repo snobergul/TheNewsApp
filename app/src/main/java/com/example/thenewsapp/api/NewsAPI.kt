5e040d0b7217d6fc68845d07ac7cbf5864fda3cc
snobergul <snobergulnaz020@gmail.com>
2024-01-26 13:37:22 +0500

    friday commit

---
+++ app/src/main/java/com/example/thenewsapp/api/NewsAPI.kt
@@ -0,0 +1,30 @@
+package com.example.thenewsapp.api
+
+import com.example.thenewsapp.models.NewsResponse
+import com.example.thenewsapp.util.Constants.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsAPI {
+
+    @GET("v2/top-headlines")
+    suspend fun getHeadlines(
+        @Query("country")
+        countryCode: String = "pk",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY,
+    ):Response<NewsResponse>
+
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("q")
+        searchQuery: String,
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ):Response<NewsResponse>
+}
---
+++ app/src/main/java/com/example/thenewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.thenewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "cae0232a468c451fb180d2256bf79a2b"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = "500L"
+        const val QUERY_PAGE_SIZE = "20"
+    }
+}
