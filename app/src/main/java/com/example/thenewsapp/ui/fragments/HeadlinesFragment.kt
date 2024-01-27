f18f365856ac4a68427be9d16f37e85b83a8f2d5
snobergul <snobergulnaz020@gmail.com>
2024-01-27 12:23:58 +0500

    saturday commit

--- app/src/main/java/com/example/thenewsapp/ui/fragments/HeadlinesFragment.kt
+++ app/src/main/java/com/example/thenewsapp/ui/fragments/HeadlinesFragment.kt
@@ -1,12 +1,102 @@
 package com.example.thenewsapp.ui.fragments
 
 import android.os.Bundle
+import android.provider.SyncStateContract.Constants
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
+import android.widget.Button
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.thenewsapp.R
+import com.example.thenewsapp.adapters.NewsAdapter
+import com.example.thenewsapp.databinding.FragmentHeadlinesBinding
+import com.example.thenewsapp.ui.NewsViewModel
 
 class HeadlinesFragment : Fragment() {
 
+    lateinit var newsViewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+    lateinit var retryButton: Button
+    lateinit var errorText: TextView
+    lateinit var itemHeadlinesError: CardView
+
+    lateinit var binding: FragmentHeadlinesBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHeadlinesBinding.bind(view)
+    }
+
+    var isError = false
+    var isLoading = false
+    var isLastPage = false
+    var isScrolling = false
+
+    private fun hideProgressBar() {
+        binding.paginationProgressBar.visibility = View.INVISIBLE
+        isLoading = false
+    }
+
+    private fun showProgressBar() {
+        binding.paginationProgressBar.visibility = View.VISIBLE
+        isLoading = true
+    }
+
+    private fun hideErrorMessage() {
+        itemHeadlinesError.visibility = View.INVISIBLE
+        isError = false
+    }
+
+    private fun showErrorMessage(message: String) {
+        itemHeadlinesError.visibility = View.VISIBLE
+        errorText.text = message
+        isError = true
+    }
+
+    val scrollerListener = object : RecyclerView.OnScrollListener() {
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+
+            val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+            val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+            val visibleItemCount = layoutManager.childCount
+            val totalItemCount = layoutManager.itemCount
+
+            val isNoErrors = !isError
+            val isNotLoadingAndLastPage = !isLoading && !isLastPage
+            val isAtLastItem = firstVisibleItemPosition + visibleItemCount >= totalItemCount
+            val isNotAtBeginning = firstVisibleItemPosition >= 0
+            val isTotalMoreThanVisible =
+                totalItemCount >= com.example.thenewsapp.util.Constants.QUERY_PAGE_SIZE
+            val shouldPaginate =
+                isNoErrors && isNotLoadingAndLastPage && isAtLastItem && isNotAtBeginning && isTotalMoreThanVisible && isScrolling
+            if (shouldPaginate) {
+                newsViewModel.getHeadlines("pk")
+                isScrolling = false
+            }
+        }
+
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+
+            if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                isScrolling = true
+            }
+        }
+    }
+
+    private fun setupHeadlinesRecycler() {
+        newsAdapter = NewsAdapter()
+        binding.recyclerHeadlines.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+            addOnScrollListener(this@HeadlinesFragment.scrollListener)
+
+        }
+    }
 }
--- app/src/main/java/com/example/thenewsapp/util/Constants.kt
+++ app/src/main/java/com/example/thenewsapp/util/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
     companion object{
         const val API_KEY = "cae0232a468c451fb180d2256bf79a2b"
         const val BASE_URL = "https://newsapi.org/"
-        const val SEARCH_NEWS_TIME_DELAY = "500L"
-        const val QUERY_PAGE_SIZE = "20"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
